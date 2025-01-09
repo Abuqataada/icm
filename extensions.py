a4c28e24ad4e5b167489d319a8692f984a4bba08
Yusuf <abuqataada21@gmail.com>
2025-01-09 15:41:47 +0100

    Updated the db

--- extensions.py
+++ extensions.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 # Configure SQLite database
 app.config['SECRET_KEY'] = 'nhjvsnjtrhgv,nvknryvn4wt7tvt4ttnsnhj$362$#@^$#@h#$$#&$GYCGHSGHDcgcegcsfgyes'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://avnadmin:AVNS_LxUTMKMU7GW7gwm1sas@postgresdb-abuqataada21-54f9.d.aivencloud.com:23198/defaultdb?sslmode=require'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Configure upload folder for images
