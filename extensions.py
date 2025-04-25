ebbe15d57cffd2303fae806dfe72c77191e8b5f5
Yusuf Isah <abuqattada21@gmail.com>
2025-04-25 12:31:18 +0100

    Added the aiven database

--- extensions.py
+++ extensions.py
@@ -1,17 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import pymysql
 import os
 
 app = Flask(__name__)
 
 # Configure SQLite database
-app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('ICM_DB_URI')
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "for_development")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("ICM_DB_URI", "sqlite:///users.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Disable debug mode in production
+app.config["DEBUG"] = os.getenv("FLASK_ENV", "production") == "development"
 
 # Configure upload folder for images
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+pymysql.install_as_MySQLdb()
 
 db = SQLAlchemy(app)
 
