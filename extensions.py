fa5164dd50c687ac69e0fa07ffc5b79334d74217
Yusuf Isah <abuqattada21@gmail.com>
2025-04-30 12:31:18 +0100

    Fixed some bugs and recover all my missing files

--- extensions.py
+++ extensions.py
@@ -1,15 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+<<<<<<< HEAD
 import pymysql
 from flask_migrate import Migrate
 
+=======
+>>>>>>> ed55138 (Updated the db)
 import os
 
 app = Flask(__name__)
 
 # Configure SQLite database
+<<<<<<< HEAD
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "for_development")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("ICM_DB_URI", "sqlite:///users.db")
+=======
+app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('ICM_DB_URI')
+#app.config['SECRET_KEY'] = 'nhjvsnjtrhgv,nvknryvn4wt7tvt4ttnsnhj$362$#@^$#@h#$$#&$GYCGHSGHDcgcegcsfgyes'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://avnadmin:AVNS_LxUTMKMU7GW7gwm1sas@postgresdb-abuqataada21-54f9.d.aivencloud.com:23198/defaultdb?sslmode=require'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+>>>>>>> ed55138 (Updated the db)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Disable debug mode in production
 app.config["DEBUG"] = os.getenv("FLASK_ENV", "production") == "development"
