1e25d5857d9fe004f612f2b3df48e9ca11e0fd45
Yusuf Isah <abuqattada21@gmail.com>
2025-05-02 11:38:32 +0100

    Updated the database URI

--- extensions.py
+++ extensions.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 # Configure SQLite database
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "for_development")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("ICM_DB_URI", "sqlite:///users.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Disable debug mode in production
 app.config["DEBUG"] = os.getenv("FLASK_ENV", "production") == "development"
